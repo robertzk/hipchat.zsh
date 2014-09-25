30f132811c7bf0bf150e76f3fb914a430ea99e9b
RobertZK <technoguyrob@gmail.com>
2014-09-25 01:13:50 -0500

    Use v1 api for rooms

--- hipchat.plugin.zsh
+++ hipchat.plugin.zsh
@@ -3,6 +3,21 @@
 # Dependencies: curl
 # https://github.com/robertzk/hipchat.zsh
 
+urlencode() { # http://stackoverflow.com/a/10797966/2540303
+  local data
+  if [[ $# != 1 ]]; then
+      echo "Usage: $0 string-to-urlencode"
+      return 1
+  fi
+  data="$(curl -s -o /dev/null -w %{url_effective} --get --data-urlencode "$1" "")"
+  if [[ $? != 3 ]]; then
+      echo "Unexpected error" 1>&2
+      return 2
+  fi
+  echo "${data##/?}"
+  return 0
+}
+
 hipchat_usage() {
   echo "Usage: hipchat [-d] <email or room> <message>"
   echo "-d: debug (more verbose output)"
@@ -40,19 +55,27 @@ hipchat() { # Arg 1: Username to send, rest: message to send
   if [[ $1 =~ '@' ]]; then 
     local hipchat_module='user'
     local hipchat_path='message'
+
+    local url="http://api.hipchat.com/v2/$hipchat_module/$1/$hipchat_path?auth_token=$HIPCHAT_API_TOKEN" 
+    local message="{\"message\": \"${@:2}\", \"message_format\": \"text\" }"
+    
+    if $DEBUG; then
+      echo "URL=$url"
+      echo "MESSAGE=$message"
+    fi
+    curl -H "Content-Type: application/json" --request POST $url --data $message
   else
-    local hipchat_module='room'
-    local hipchat_path='notification'
+    local from=$HIPCHAT_FROM
+    if [[ -z $from ]]; then
+      local from=`whoami`
+    fi
+    local url="http://api.hipchat.com/v1/rooms/message?format=json&auth_token=$HIPCHAT_API_TOKEN&message_format=text"
+    local message="message=`urlencode "${@:2}"`&room_id=`urlencode "$1"`&from=`urlencode "$from"`"
+    if $DEBUG; then
+      echo "URL=$url"
+      echo "MESSAGE=$message"
+    fi
+    curl --request POST $url --data $message
   fi
-
-  local url="http://api.hipchat.com/v2/$hipchat_module/$1/$hipchat_path?auth_token=$HIPCHAT_API_TOKEN" 
-  local message="{\"message\": \"${@:2}\", \"message_format\": \"text\" }"
-  
-  if $DEBUG; then
-    echo "URL=$url"
-    echo "MESSAGE=$message"
-  fi
-
-  curl -H "Content-Type: application/json" --request POST $url --data $message
 }
 
